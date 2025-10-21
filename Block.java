2bed70d8d9a15fc77f9939795626d6b00183fc1d
selina fu <selina.fu88@gmail.com>
2025-10-20 23:32:39 -0700

    created blockchain, ready to start mining

---
+++ Block.java
@@ -0,0 +1,29 @@
+import java.util.Date;
+import java.security.MessageDigest;
+
+public class Block {
+
+    public String hash;
+    public String previousHash;
+    private String data;
+    private long timeStamp;
+
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calculateHash();
+
+    }
+
+    public String calculateHash() {
+        String calculatedhash = StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        data);
+        return calculatedhash;
+
+    }
+
+
+}
