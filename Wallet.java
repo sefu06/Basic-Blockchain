ac44b907e8396c84b408d7127afe06af7849d6d9
selina fu <selina.fu88@gmail.com>
2025-10-21 23:37:56 -0700

    implemented generateKey function

--- Wallet.java
+++ Wallet.java
@@ -1,8 +1,31 @@
 
 import java.security.*;
+import java.security.spec.ECGenParameterSpec;
 
 public class Wallet {
     public PrivateKey privateKey; //only for the owner, so only they can spend their coins
     public PublicKey publicKey; //ok to share with everyone (address)
+
+    public Wallet() {
+        generateKeyPair();
+    }
+
+    public void generateKeyPair() {
+
+        try {
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+			ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+			// Initialize the key generator and generate a KeyPair
+			keyGen.initialize(ecSpec, random);   //256 bytes provides an acceptable security level
+	        	KeyPair keyPair = keyGen.generateKeyPair();
+	        	// Set the public and private keys from the keyPair
+	        	privateKey = keyPair.getPrivate();
+	        	publicKey = keyPair.getPublic();
+		}catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+    }
 }
+
 
